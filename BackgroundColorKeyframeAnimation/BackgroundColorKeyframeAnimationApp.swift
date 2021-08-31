cb3624b23355d4629388008d8bf84be3b1d37b1b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-08-31 21:33:42 +0300

    Initial Commit

---
+++ BackgroundColorKeyframeAnimation/BackgroundColorKeyframeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundColorKeyframeAnimationApp.swift
+//  BackgroundColorKeyframeAnimation
+//
+//  Created by ramil on 31.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundColorKeyframeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
