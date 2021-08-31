292a71588d4231157f82b27fc29cde98796c7dfb
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-08-31 21:38:40 +0300

    init

--- BackgroundColorKeyframeAnimation/ContentView.swift
+++ BackgroundColorKeyframeAnimation/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var switchColor = Color(.red)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            if #available(iOS 15.0, *) {
+                Rectangle()
+                    .ignoresSafeArea()
+                    .foregroundColor(switchColor)
+                    .task {
+                        withAnimation(Animation.easeInOut(duration: 1).delay(2)) {
+                            switchColor = Color(.yellow)
+                        }
+                        withAnimation(Animation.easeInOut(duration: 1).delay(4)) {
+                            switchColor = Color(.blue)
+                        }
+                        withAnimation(Animation.easeInOut(duration: 1).delay(6)) {
+                            switchColor = Color(.orange)
+                        }
+                        withAnimation(Animation.easeInOut(duration: 1).delay(8)) {
+                            switchColor = Color(.green)
+                        }
+                        withAnimation(Animation.easeInOut(duration: 1).delay(10)) {
+                            switchColor = Color(.purple)
+                        }
+                        withAnimation(Animation.easeInOut(duration: 1).delay(12)) {
+                            switchColor = Color(.red)
+                        }
+                    }
+            } else {
+                // Fallback on earlier versions
+            }
+        }
     }
 }
 
